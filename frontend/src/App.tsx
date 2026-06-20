feb9ebe3fe62ebcf6cefd3c3542faa996be074c5
mojjam07 <mojjam07@gmail.com>
2026-06-20 16:25:30 +0100

    Add build

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import { LanguageProvider } from './components/i18n/LanguageProvider'
 import AppRouter from './app/Router'
 import DirWrapper from './components/i18n/DirWrapper'
 
+// 
+
 export default function App() {
   return (
     <BrowserRouter>
